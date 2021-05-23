5b6c8d4fab702caabd5040bdf5e77acf3b95f22c
Haantzy <82786716+Haantzy@users.noreply.github.com>
2021-05-23 17:52:39 +1000

    Update OnBoard.py

--- OnBoard.py
+++ OnBoard.py
@@ -268,7 +268,7 @@ def capture_image(arucoDict,arucoParams, vid_stream):
 	while (frame_num <= max_frames) and (no_marker):
 		frame = vid_stream.read()	
 		#Send an abort command, as the ground was reached
-		m = np.mean(self.frame)	
+		m = np.mean(frame)	
 		print(m)
 		if m < 40:
 			#Check if the image is dark, which means craft is on the ground
